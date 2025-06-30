fe5ed70fe2a49e1d73d2ec24c2cad980931d12e7
minsu-jeong123 <81951299+minsu-jeong123@users.noreply.github.com>
2025-06-30 04:37:41 +0000

    user 컨트롤러 v1

--- subscriber/src/main/java/aibook/infra/UserController.java
+++ subscriber/src/main/java/aibook/infra/UserController.java
@@ -7,12 +7,11 @@ import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 //<<< Clean Arch / Inbound Adaptor
 
 @RestController
+@RequestMapping("/users")
 @Transactional
 public class UserController {
 
