756b01c275fb81a651ba4e9949b327ad89b08174
LChaeHyun <94784148+LChaeHyun@users.noreply.github.com>
2025-07-03 06:31:30 +0000

    index에 mypage 연결

--- front/src/router/index.tsx
+++ front/src/router/index.tsx
@@ -5,6 +5,7 @@ import { LibraryList } from "../features/Library/LibraryList";
 import ManuscriptEditor from "../features/Manuscript/ManuscriptEditor";
 import SignupPage from "../features/Auth/SignupPage";
 import LoginPage from "../features/Auth/LoginPage";
+import MyPage from "../features/User/MyPage"
 import BuySubscriptionButton from "../features/User/BuySubscription";
 import BuyPointButton from "../features/User/BuyPoint";
 import AuthorRegistration from "../features/AuthorRegistration/pages/AuthorRegistration";
@@ -30,16 +31,7 @@ export const router = createBrowserRouter([
       },
       {
         path: "mypage",
-        element: (
-          <div>
-            <h2>마이페이지</h2>
-            <p>여기에 회원 정보나 구독 정보가 표시됩니다.</p>
-            <Stack spacing={4} mt={4}>
-              <BuySubscriptionButton userId={1} />
-              <BuyPointButton userId={1} />
-            </Stack>
-          </div>
-        ),
+        element: <MyPage />,
       },
       {
         path: "manuscripts/new",
