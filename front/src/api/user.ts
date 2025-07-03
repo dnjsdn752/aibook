e5133e21849cc4102d3cffce719b537e8b08a75e
LChaeHyun <94784148+LChaeHyun@users.noreply.github.com>
2025-07-03 06:22:50 +0000

    mypage 구현

--- front/src/api/user.ts
+++ front/src/api/user.ts
@@ -4,17 +4,37 @@ import axios from "axios";
  * 구독자 회원가입 API
  * 백엔드 구독자관리 서비스의 엔드포인트에 POST 요청
  */
+
+const api = axios.create({
+  baseURL: 'https://special-potato-5pwv9q4594g27r76-8088.app.github.dev', // Gateway 주소
+});
+
 export const signupUser = (data: {
   email: string;
   password: string;
   userName: string;
 }) => {
-  return axios.post(
-    "https://8088-dnjsdn752-aibook-kcc5uifn888.ws-us120.gitpod.io/users",
+  return api.post(
+    "/users",
     {
       ...data,
       isSubscription: false, // ⭐ 기본값
     }
   );
 };
+
+export const myData = (userId: number) => {
+  return api.get(`/users/${userId}`);
+};
+
+export const myPoint = (userId: number) => {
+  return api.get(`/points/${userId}`);
+};
+
+export const myReading = (userId: number) => {
+  return api.get(`/readings/${userId}`);
+};
+
+
+
 
